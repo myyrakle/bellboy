a2a7dfc56f0a7ef8edbd761c5cf84faa3195885c
myyrakle <sssang97@naver.com>
2026-02-10 02:03:27 +0900

    로깅 환경 구성

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,31 @@ mod detector;
 mod notifier;
 mod state;
 
+use std::env;
+
 use detector::detect_changes;
 use futures::TryStreamExt;
 use k8s_openapi::api::apps::v1::Deployment;
 use kube::{
-    runtime::{watcher, WatchStreamExt},
     Api, Client,
+    runtime::{WatchStreamExt, watcher},
 };
 use notifier::notify;
 use state::StateManager;
 
+fn setup_logging() {
+    unsafe {
+        if env::var("RUST_LOG").is_err() {
+            env::set_var("RUST_LOG", "info");
+        }
+    }
+    env_logger::init();
+}
+
 #[tokio::main]
 async fn main() {
+    setup_logging();
+
     let client = Client::try_default()
         .await
         .expect("Failed to create client");
